a8ff562b2499aa56dd0888234e2223e871bbe77e
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-05 06:12:51 +0300

    Add companies.swagger.ts Swagger documentation

---
+++ backend/src/swagger/companies.swagger.ts
@@ -0,0 +1,90 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Companies
+ *   description: Company management
+ */
+
+/**
+ * @swagger
+ * /companies:
+ *   get:
+ *     summary: Get all companies
+ *     tags: [Companies]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: List of companies
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiResponse'
+ *   post:
+ *     summary: Create a new company
+ *     tags: [Companies]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Company'
+ *     responses:
+ *       201:
+ *         description: Company created
+ */
+
+/**
+ * @swagger
+ * /companies/{id}:
+ *   get:
+ *     summary: Get company by ID
+ *     tags: [Companies]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Company found
+ *   put:
+ *     summary: Update company
+ *     tags: [Companies]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Company'
+ *     responses:
+ *       200:
+ *         description: Company updated
+ *   delete:
+ *     summary: Delete company
+ *     tags: [Companies]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Company deleted
+ */
