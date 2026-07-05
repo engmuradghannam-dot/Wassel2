aea78d6f604c83e5edf1436d0d0ab05213446314
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-05 06:13:04 +0300

    Add warehouses.swagger.ts Swagger documentation

---
+++ backend/src/swagger/warehouses.swagger.ts
@@ -0,0 +1,95 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Warehouses
+ *   description: Warehouse management
+ */
+
+/**
+ * @swagger
+ * /warehouses:
+ *   get:
+ *     summary: Get all warehouses
+ *     tags: [Warehouses]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: companyId
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of warehouses
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiResponse'
+ *   post:
+ *     summary: Create a new warehouse
+ *     tags: [Warehouses]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Warehouse'
+ *     responses:
+ *       201:
+ *         description: Warehouse created
+ */
+
+/**
+ * @swagger
+ * /warehouses/{id}:
+ *   get:
+ *     summary: Get warehouse by ID
+ *     tags: [Warehouses]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Warehouse found
+ *   put:
+ *     summary: Update warehouse
+ *     tags: [Warehouses]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Warehouse'
+ *     responses:
+ *       200:
+ *         description: Warehouse updated
+ *   delete:
+ *     summary: Delete warehouse
+ *     tags: [Warehouses]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Warehouse deleted
+ */
