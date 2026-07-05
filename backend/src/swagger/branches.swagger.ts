3e22dc402ebea7d5e5df46aab161ff8ed94578d4
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-05 06:13:02 +0300

    Add branches.swagger.ts Swagger documentation

---
+++ backend/src/swagger/branches.swagger.ts
@@ -0,0 +1,95 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Branches
+ *   description: Branch management
+ */
+
+/**
+ * @swagger
+ * /branches:
+ *   get:
+ *     summary: Get all branches
+ *     tags: [Branches]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: companyId
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of branches
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiResponse'
+ *   post:
+ *     summary: Create a new branch
+ *     tags: [Branches]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Branch'
+ *     responses:
+ *       201:
+ *         description: Branch created
+ */
+
+/**
+ * @swagger
+ * /branches/{id}:
+ *   get:
+ *     summary: Get branch by ID
+ *     tags: [Branches]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Branch found
+ *   put:
+ *     summary: Update branch
+ *     tags: [Branches]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Branch'
+ *     responses:
+ *       200:
+ *         description: Branch updated
+ *   delete:
+ *     summary: Delete branch
+ *     tags: [Branches]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Branch deleted
+ */
