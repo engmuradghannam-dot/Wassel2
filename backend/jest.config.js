dc3c69ee6c87e997fd5a5f79ab48c5f315f6d628
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-05 06:15:02 +0300

    Update jest.config.js with comprehensive test configuration

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,4 +4,20 @@ module.exports = {
   testEnvironment: 'node',
   passWithNoTests: true,
   testMatch: ['**/*.test.ts', '**/*.spec.ts'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts',
+    '!src/swagger.ts',
+    '!src/server.ts',
+  ],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov', 'html'],
+  verbose: true,
+  testTimeout: 30000,
+  clearMocks: true,
+  restoreMocks: true,
 };
