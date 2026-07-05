4fe5917fc9b9d0fef70373a104522c00c3c183be
engmuradghannam-dot <eng.murad.ghannam@gmail.com>
2026-07-05 06:12:54 +0300

    Add suppliers.swagger.ts Swagger documentation

---
+++ backend/src/swagger/suppliers.swagger.ts
@@ -0,0 +1,99 @@
+/**
+ * @swagger
+ * tags:
+ *   name: Suppliers
+ *   description: Supplier management
+ */
+
+/**
+ * @swagger
+ * /suppliers:
+ *   get:
+ *     summary: Get all suppliers
+ *     tags: [Suppliers]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: companyId
+ *         schema:
+ *           type: string
+ *       - in: query
+ *         name: search
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of suppliers
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/ApiResponse'
+ *   post:
+ *     summary: Create a new supplier
+ *     tags: [Suppliers]
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Supplier'
+ *     responses:
+ *       201:
+ *         description: Supplier created
+ */
+
+/**
+ * @swagger
+ * /suppliers/{id}:
+ *   get:
+ *     summary: Get supplier by ID
+ *     tags: [Suppliers]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Supplier found
+ *   put:
+ *     summary: Update supplier
+ *     tags: [Suppliers]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Supplier'
+ *     responses:
+ *       200:
+ *         description: Supplier updated
+ *   delete:
+ *     summary: Delete supplier
+ *     tags: [Suppliers]
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Supplier deleted
+ */
